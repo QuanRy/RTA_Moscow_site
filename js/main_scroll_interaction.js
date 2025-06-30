896fafd55f5e14f7857c8363507c1683719c2e6e
QuanRy <kacman1964@mail.ru>
2025-06-30 23:17:00 +0300

    Сделал под первую страничку таблицы критерии. (Без базовой частоты, без результата высчитываемого, без кнопки расчитать, без выбора компании и сегментов)

--- js/main_scroll_interaction.js
+++ js/main_scroll_interaction.js
@@ -1,14 +1,14 @@
-for (let i = 1; i <= 5; i++) {
-    const slider = document.getElementById(`slider${i}`);
-    const input = document.getElementById(`value${i}`);
+// for (let i = 1; i <= 5; i++) {
+//     const slider = document.getElementById(`slider${i}`);
+//     const input = document.getElementById(`value${i}`);
 
-    slider.addEventListener("input", () => {
-        input.value = slider.value;
-    });
+//     slider.addEventListener("input", () => {
+//         input.value = slider.value;
+//     });
 
-    input.addEventListener("input", () => {
-        if (input.value >= -5 && input.value <= 5) {
-            slider.value = input.value;
-        }
-    });
-}
+//     input.addEventListener("input", () => {
+//         if (input.value >= -5 && input.value <= 5) {
+//             slider.value = input.value;
+//         }
+//     });
+// }
