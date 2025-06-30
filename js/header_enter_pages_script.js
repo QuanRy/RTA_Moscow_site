202fe56f36f2137d1a79738bce991543400b8df1
QuanRy <kacman1964@mail.ru>
2025-06-30 18:56:13 +0300

    добавил переадресацию между страничками по кнопкам хэдера, добавил сами кнопки хэдера, добавил коды js  для обработки нажатий и переадресации

---
+++ js/header_enter_pages_script.js
@@ -0,0 +1,11 @@
+document.querySelector('.auth-button.registration').addEventListener('click', () => {
+    window.location.href = '../html/registration_only_admin.html';
+});
+
+document.querySelector('.auth-button.login').addEventListener('click', () => {
+    window.location.href = '../html/login.html';
+});
+
+document.querySelector('.logo-container').addEventListener('click', () => {
+    window.location.href = '../html/main_window.html';
+});
