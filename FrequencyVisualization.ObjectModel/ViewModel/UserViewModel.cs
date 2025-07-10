81bed6703bcaf7e31f954a2a009fa33188855997
KateVlg <ekaterina.magnitsckaya@ya.ru>
2025-07-11 00:25:39 +0300

    Исправлены ошибки

---
+++ FrequencyVisualization.ObjectModel/ViewModel/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequencyVisualization.ObjectModel.ViewModel
+{
+    public class UserViewModel : BaseModel
+    {
+        public string TokenUser { get; set; }
+    }
+}
