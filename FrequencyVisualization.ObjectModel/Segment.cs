d0f1d317a397c475ef2c57be94ca78fe58c1c17a
QuanRy <kacman1964@mail.ru>
2025-07-17 22:40:22 +0300

    вот это решение помогло исправить ошибки с сегментом, и получению компании по Id

--- FrequencyVisualization.ObjectModel/Segment.cs
+++ FrequencyVisualization.ObjectModel/Segment.cs
@@ -1,4 +1,8 @@
-﻿namespace FrequencyVisualization.ObjectModel
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FrequencyVisualization.ObjectModel
 {
     public class Segment : BaseModel
     {
@@ -10,6 +14,12 @@
 
         public List<FactorValue> FactorValues { get; set; }
 
-        // public CompanyItemId { get; set; }
+        // ВОТ ДОБАВИЛ ЭТО + USING (сверху их не было), иначе нет информации в сегменте, какой компании он принадлежит
+        // =======================================================================================
+        [ForeignKey("Company")]  // указываем, что это внешний ключ на Company
+        public Guid CompanyItemId { get; set; }
+
+        public Company Company { get; set; }
+        // =======================================================================================
     }
 }
